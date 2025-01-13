fb9482c194d3ace2856753147cdf0be5bf72c8bd
Josefina Estevez <josefinaestevez91@gmail.com>
2025-01-13 15:22:21 +0100

    init nuxt + tailwindcss project

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="min-h-screen flex flex-col">
+      <main class="flex-grow">
+        <slot />
+      </main>
+      <footer>
+        <slot name="footer">
+          <!-- <Footer /> -->
+        </slot>
+      </footer>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLayout name="default">
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+    </h1>
+  </NuxtLayout>
+</template>
